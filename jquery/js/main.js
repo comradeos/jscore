8308b98a8636dc5df16f317d2e89eac18e4e93e2
Iaroslav Os <osmnbx@gmail.com>
2022-07-30 15:25:10 +0300

    added add files

---
+++ jquery/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+   $(':submit').click(function(e) {
+      $('.text2').attr('disabled','disabled');
+      e.preventDefault();
+   });
+   $(':reset').click(function(e) {
+      $('.text2').removeAttr('disabled');
+      e.preventDefault();
+   });
+});
