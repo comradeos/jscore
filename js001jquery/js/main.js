fcd90ecb4d49b54f586cf60e64fe087efc215442
Iaroslav Os <osmnbx@gmail.com>
2022-08-14 13:41:50 +0300

    added/updated

--- js001jquery/js/main.js
+++ js001jquery/js/main.js
@@ -7,7 +7,9 @@ $('a');
 
 $('nav menu li');
 $('.elm_class > div');
-$('img + h4');
+$('img + h4'); // все img после которых h4
 
-$('img[height=200]');
-$('img[src^=img/]');
+$('img[height=200]'); // все img с аттрибутом height="200" 
+$('img[src^=img/]'); // аттрибут src начинается с img/
+$('img[src$=.png]'); // аттрибут src заканчивается на .png 
+$('img[src*=№]'); // аттрибут src содержит символ # 
