dd83fd46d4ceb789b6b3120cac71642c916f8784
Iaroslav Os <osmnbx@gmail.com>
2023-03-18 17:20:15 +0200

    added/updated

--- js004vue31/src/App.vue
+++ js004vue31/src/App.vue
@@ -1,9 +1,29 @@
 <template>
-
+    <div>
+        <button @click="addLike">Like</button>
+        <button @click="addDislike">Dislike</button>
+    </div>
+    <div>Количество лайков: <strong>{{likes}}</strong></div>
+    <div>Количество дизлайков: <strong>{{dislikes}}</strong></div>
 </template>
 
 <script>
-
+    export default {
+        data() {
+            return {
+                likes: 0,
+                dislikes: 0,
+            }
+        },
+        methods: {
+            addLike() {
+                this.likes += 1;
+            },
+            addDislike() {
+                this.dislikes += 1;
+            }
+        }
+    }
 </script>
 
 <style>
