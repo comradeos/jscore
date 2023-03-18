f427efe01e1bd04ac2361bd81ebfff8e6663e8a3
Iaroslav Os <osmnbx@gmail.com>
2023-03-18 18:06:18 +0200

    added/updated

--- js004vue31/src/App.vue
+++ js004vue31/src/App.vue
@@ -1,31 +1,58 @@
 <template>
-    <div>
-        <button @click="addLike">Like</button>
-        <button @click="addDislike">Dislike</button>
+
+    <form>
+        <input
+            v-bind:value="title"
+            @input="inputTitle"
+            type="text"
+            placeholder="Описание"
+        />
+
+        <input v-bind:value="body" type="text" placeholder="Назване"/>
+        <button @click="addPost">Создать</button>
+    </form>
+
+    <div class="post" v-for="post in posts">
+        <div><strong>Название:</strong> {{post.title}}:</div>
+        <div><strong>Описание:</strong> {{post.body}}:</div>
     </div>
-    <div>Количество лайков: <strong>{{likes}}</strong></div>
-    <div>Количество дизлайков: <strong>{{dislikes}}</strong></div>
+
 </template>
 
 <script>
     export default {
         data() {
             return {
-                likes: 0,
-                dislikes: 0,
+                posts: [
+                    {id: 1, title: 'Пост 1', body: 'Описание поста 1'},
+                    {id: 2, title: 'Пост 2', body: 'Описание поста 2'},
+                    {id: 3, title: 'Пост 3', body: 'Описание поста 3'},
+                ],
+                title: '',
+                body: '',
             }
         },
         methods: {
-            addLike() {
-                this.likes += 1;
+            addPost() {
+
             },
-            addDislike() {
-                this.dislikes += 1;
+            inputTitle(event) {
+                console.log(event.target.value);
             }
         }
     }
 </script>
 
 <style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .post, form {
+        padding: 15px;
+        border: 2px solid teal;
+        margin-top: 15px;
+    }
 
 </style>
