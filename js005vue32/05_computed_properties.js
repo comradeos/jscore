94c6dad3a0cf60626d68c368df98f72bdb7ae994
Iaroslav Os <osmnbx@gmail.com>
2023-08-24 20:43:22 +0300

    updated

--- js005vue32/05_computed_properties.js
+++ js005vue32/05_computed_properties.js
@@ -13,9 +13,17 @@ const app = Vue.createApp({
     methods: {
         toggleFav(book) {
             book.favorite = !book.favorite;
-        }
-    }
+        },
+    },
+
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.favorite == true);
+        },
+    },
 
 });
 
 app.mount("#app")
+
+// https://www.youtube.com/watch?v=CYPZBK8zUik&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=6
