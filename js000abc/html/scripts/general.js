2d2ecdd930617bb8e5a3d657f3d1cd9a32f39e30
Iaroslav Os <osmnbx@gmail.com>
2023-10-26 17:42:51 +0300

    8495

--- js000abc/html/scripts/general.js
+++ js000abc/html/scripts/general.js
@@ -1,3 +1,36 @@
 "use strict";
 
-console.log('general.js')
+console.log('general.js');
+
+class Car {
+    constructor(brand) {
+        this.brand = brand;
+    }
+}
+
+class Model extends Car {
+    constructor(brand, color) {
+        super(brand);
+        this.color = color;
+    }
+    showInfo(){
+        console.log(`Car->Model: ${this.brand} in ${this.color} color!`);
+    }
+}
+
+let model = new Model("BMW", "black");
+
+class PopUp {
+    constructor(method, h1Text, buttonText) {
+        this.content = `
+            <div class="popup">
+                <form method="${method}" action="#">
+                    <h1>${h1Text}</h1><br>
+                    <label for="id"></label>
+                    <input type="text" name="text" id="id"><br>
+                    <button>${buttonText}</button>
+                </form>
+            </div>`;
+    }
+}
+
